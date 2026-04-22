8481518ab9279e3067d661445485dbaa16f4eebf
Fabian Zamora <fabianz66@gmail.com>
2026-04-22 10:58:46 -0600

    Add jobLoader.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import JobsPage from './pages/JobsPage.jsx'
-import JobPage from './pages/JobPage.jsx'
+import JobPage, {jobLoader} from './pages/JobPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
 import MainLayout from './layouts/MainLayout.tsx'
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/jobs" element={<JobsPage />} />
-          <Route path="/jobs/:id" element={<JobPage />} />
-          {/* Catch all other routes */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </Router>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+      {/* Catch all other routes */}
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
   )
+);
+
+function App() {
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,13 +1,25 @@
-import { useParams } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
+import { FaArrowLeft, FaMapMarker } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const JobPage = () => {
-  const { id } = useParams()
+  const job = useLoaderData();
 
   return (
-    <h1 className="text-3xl font-bold text-center mt-10">
-      Job Details for ID: {id}
-    </h1>
+    <section className="container m-auto py-10 px-6">
+      <h1 className="text-3xl font-bold">{job.title}</h1>
+      <p className="text-gray-500">{job.type}</p>
+    </section>
   )
 }
 
-export default JobPage
+const jobLoader = async ({ params }) => {
+  const response = await fetch(`/api/jobs/${params.id}`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch job details');
+  }
+  const job = await response.json();
+  return job;
+};
+
+export {JobPage as default, jobLoader};
