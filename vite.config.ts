86e1c675fcdba68f98f5204c37135ab4113240de
Fabian Zamora <fabianz66@gmail.com>
2026-04-22 19:49:03 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
-        changeOrigin: false,
+        changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     }
